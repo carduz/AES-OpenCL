5a5efa057749599540ee8e2206de357a7cbb9318
tux-mind <massimo.dragano@gmail.com>
2016-06-29 01:10:01 +0200

    [cosmetics] reformat code

--- main.c
+++ main.c
@@ -9,45 +9,44 @@
 
 #include <sys/time.h>
 
-
 #define MB			(1024 * 1024)
 #define TOTAL_LEN	(1* 1024UL * MB)
 
 void run(const char *name, unsigned char *buf, int len) {
-    int pass = TOTAL_LEN / len;
+  int pass = TOTAL_LEN / len;
 
-    unsigned char key[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-                             0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
+  unsigned char key[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+                            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
 
 
-    int outlen = 0;
-    struct timeval start, end;
-    double total_time, throughput;
+  int outlen = 0;
+  struct timeval start, end;
+  double total_time, throughput;
 
-    EVP_CIPHER_CTX ctx;
-    EVP_CIPHER_CTX_init(&ctx);
+  EVP_CIPHER_CTX ctx;
+  EVP_CIPHER_CTX_init(&ctx);
 
-    EVP_CIPHER *cipher = (EVP_CIPHER *) EVP_get_cipherbyname("aes-256-ecb");
+  EVP_CIPHER *cipher = (EVP_CIPHER *) EVP_get_cipherbyname("aes-256-ecb");
 
-    EVP_CipherInit_ex(&ctx, cipher, NULL, key, NULL, 1);
+  EVP_CipherInit_ex(&ctx, cipher, NULL, key, NULL, 1);
 
-    gettimeofday(&start, NULL);
-    for (int i = 0; i < pass; i++) {
-        fprintf(stderr, "%s: Pass %d/%d\n", name, i + 1, pass);
-        // note we cannot run multiple pass on the same buffer, the CPU will cache it! (smart bastards at Intel)
-        EVP_CipherUpdate(&ctx, buf + i * len, &outlen, buf, len);
-        if (len != outlen) {
-            fprintf(stderr, "Fatal error: incorrect output size.\n");
-        }
+  gettimeofday(&start, NULL);
+  for (int i = 0; i < pass; i++) {
+    fprintf(stderr, "%s: Pass %d/%d\n", name, i + 1, pass);
+    // note we cannot run multiple pass on the same buffer, the CPU will cache it! (smart bastards at Intel)
+    EVP_CipherUpdate(&ctx, buf + i * len, &outlen, buf, len);
+    if (len != outlen) {
+      fprintf(stderr, "Fatal error: incorrect output size.\n");
     }
+  }
 
-    gettimeofday(&end, NULL);
-    total_time = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec) / 1000000.0;
-    throughput = TOTAL_LEN * 8 / total_time / 1000000;
-    printf("%s: %u-byte blocks, %lubytes in %f s, Throughput: %fMbps\n", name, len, TOTAL_LEN, total_time, throughput);
+  gettimeofday(&end, NULL);
+  total_time = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec) / 1000000.0;
+  throughput = TOTAL_LEN * 8 / total_time / 1000000;
+  printf("%s: %u-byte blocks, %lubytes in %f s, Throughput: %fMbps\n",
+         name, len, TOTAL_LEN, total_time, throughput);
 
-
-    EVP_CIPHER_CTX_cleanup(&ctx);
+  EVP_CIPHER_CTX_cleanup(&ctx);
 }
 
 static void put_local_work_size(char *arg) {
@@ -58,26 +57,27 @@ static void put_local_work_size(char *arg) {
 }
 
 int main(int argc, char **argv) {
-    OpenSSL_add_all_algorithms();
-    ERR_clear_error();
-    long int err;
+  OpenSSL_add_all_algorithms();
+  ERR_clear_error();
+  long int err;
 
-    int len = 128 * MB;
-    if (argc > 1) {
-        len = atoi(argv[1]) * MB;
-    }
+  int len = 128 * MB;
+  if (argc > 1) {
+    len = atoi(argv[1]) * MB;
+  }
 
-    unsigned char *buf = (unsigned char *) malloc(TOTAL_LEN);
-    if (!buf) {
-        fprintf(stderr, "Error Allocating Memory");
-    }
+  unsigned char *buf = (unsigned char *) malloc(TOTAL_LEN);
+  if (!buf) {
+    fprintf(stderr, "Error Allocating Memory");
+  }
 
-    ENGINE_load_builtin_engines();
-    //OpenSSL_add_all_algorithms();
+  ENGINE_load_builtin_engines();
+  
 #ifdef OPENCL_ENGINE
-    ENGINE *e = ENGINE_by_id("dynamic");
-    if(!e)
-        fprintf(stderr, "Failed to load OpenCL engine (1)!\n");
+  ENGINE *e = ENGINE_by_id("dynamic");
+  if(!e) {
+    fprintf(stderr, "Failed to load OpenCL engine (1)!\n");
+  }
 
 	if (!ENGINE_ctrl_cmd_string(e, "SO_PATH", OPENCL_ENGINE, 0) ||
 		!ENGINE_ctrl_cmd_string(e, "LOAD", NULL, 0)) {
@@ -90,15 +90,15 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 	ENGINE_set_default(e, ENGINE_METHOD_ALL);
-    
-    if(argc >= 3) {
-      put_local_work_size(argv[2]);
-    }
+  
+  if(argc >= 3) {
+    put_local_work_size(argv[2]);
+  }
 
-    run(argv[0], buf, len);
+  run(argv[0], buf, len);
 
 #endif
-    free(buf);
+  free(buf);
 
-    return 0;
+  return 0;
 }
--- opencl.c
+++ opencl.c
@@ -90,109 +90,109 @@ static cl_mem buffer_state;
 static cl_mem buffer_roundkeys;
 
 static int opencl_init(ENGINE *e) {
-    printf("init");
-    cl_int err = 0;
-    cl_uint numPlatforms;
+  printf("init");
+  cl_int err = 0;
+  cl_uint numPlatforms;
 
-    err = clGetPlatformIDs(0, NULL, &numPlatforms);
-    if (err != CL_SUCCESS || !numPlatforms) {
-        fprintf(stderr, "Failed to get OpenCL platforms\n");
-        return 0;
-    }
+  err = clGetPlatformIDs(0, NULL, &numPlatforms);
+  if (err != CL_SUCCESS || !numPlatforms) {
+      fprintf(stderr, "Failed to get OpenCL platforms\n");
+      return 0;
+  }
 
-    cl_platform_id *platforms = (cl_platform_id *) malloc(sizeof(cl_platform_id) * numPlatforms);
+  cl_platform_id *platforms = (cl_platform_id *) malloc(sizeof(cl_platform_id) * numPlatforms);
 
-    err = clGetPlatformIDs(numPlatforms, platforms, NULL);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Failed to get OpenCL platforms\n");
-        return 0;
-    }
+  err = clGetPlatformIDs(numPlatforms, platforms, NULL);
+  if (err != CL_SUCCESS) {
+      fprintf(stderr, "Failed to get OpenCL platforms\n");
+      return 0;
+  }
 
-    cl_platform_id platform = platforms[0];
+  cl_platform_id platform = platforms[0];
 
-    /* print platform information */
+  /* print platform information */
 
-    cl_device_id device = NULL;
-    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "No GPU support, falling back to CPU\n");
-        err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &device, NULL);
-    }
-    if (!device) {
-        fprintf(stderr, "Error getting device\n");
-        return 0;
-    }
+  cl_device_id device = NULL;
+  err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+  if (err != CL_SUCCESS) {
+      fprintf(stderr, "No GPU support, falling back to CPU\n");
+      err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &device, NULL);
+  }
+  if (!device) {
+      fprintf(stderr, "Error getting device\n");
+      return 0;
+  }
 
-    context = clCreateContext(0, 1, &device, opencl_error, NULL, &err);
-    if (!context) {
-        fprintf(stderr, "Error creating context\n");
-        return 0;
-    }
+  context = clCreateContext(0, 1, &device, opencl_error, NULL, &err);
+  if (!context) {
+      fprintf(stderr, "Error creating context\n");
+      return 0;
+  }
 
-    queue = clCreateCommandQueue(context, device, 0, &err);
-    if (!queue) {
-        fprintf(stderr, "Error creating command queue\n");
-        return 0;
-    }
+  queue = clCreateCommandQueue(context, device, 0, &err);
+  if (!queue) {
+      fprintf(stderr, "Error creating command queue\n");
+      return 0;
+  }
 
-    const char *kernel_source = load_kernel_source(AES_KERNEL);
-    program = clCreateProgramWithSource(context, 1, &kernel_source, NULL, &err);
-    if (!program || err != CL_SUCCESS) {
-        fprintf(stderr, "Error creating program\n");
-        return 0;
-    }
+  const char *kernel_source = load_kernel_source(AES_KERNEL);
+  program = clCreateProgramWithSource(context, 1, &kernel_source, NULL, &err);
+  if (!program || err != CL_SUCCESS) {
+      fprintf(stderr, "Error creating program\n");
+      return 0;
+  }
 
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    if (err != CL_SUCCESS) {
-        size_t len;
-        char buffer[2048];
+  err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+  if (err != CL_SUCCESS) {
+      size_t len;
+      char buffer[2048];
 
-        fprintf(stderr, "Error building program\n");
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
-        fprintf(stderr, "%s\n", buffer);
-        return 0;
-    }
+      fprintf(stderr, "Error building program\n");
+      clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+      fprintf(stderr, "%s\n", buffer);
+      return 0;
+  }
 
-    encrypt_kernel = clCreateKernel(program, "AES_encrypt", &err);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Error creating encrypt kernel\n");
-        return 0;
-    }
+  encrypt_kernel = clCreateKernel(program, "AES_encrypt", &err);
+  if (err != CL_SUCCESS) {
+      fprintf(stderr, "Error creating encrypt kernel\n");
+      return 0;
+  }
 
-    if (getenv("OPENSSL_OPENCL_LOCAL_WORK_SIZE")) {
-        local_work_size = atoi(getenv("OPENSSL_OPENCL_LOCAL_WORK_SIZE"));
-    }
+  if (getenv("OPENSSL_OPENCL_LOCAL_WORK_SIZE")) {
+      local_work_size = atoi(getenv("OPENSSL_OPENCL_LOCAL_WORK_SIZE"));
+  }
 
-    if (getenv("OPENSSL_OPENCL_USE_LOCAL_T_TABLE")) {
-        local_work_size = 256; // must be this value to work
-        encrypt_kernel = clCreateKernel(program, "AES_encrypt_local", &err);
-        if (err != CL_SUCCESS) {
-            fprintf(stderr, "Error creating encrypt kernel\n");
-            return 0;
-        }
-    }
+  if (getenv("OPENSSL_OPENCL_USE_LOCAL_T_TABLE")) {
+      local_work_size = 256; // must be this value to work
+      encrypt_kernel = clCreateKernel(program, "AES_encrypt_local", &err);
+      if (err != CL_SUCCESS) {
+          fprintf(stderr, "Error creating encrypt kernel\n");
+          return 0;
+      }
+  }
 
-    int max_buffer_size = MAX_BUFFER_SIZE;
+  int max_buffer_size = MAX_BUFFER_SIZE;
 
-    if (getenv("OPENSSL_OPENCL_MAX_BUFFER_SIZE")) {
-        max_buffer_size = atoi(getenv("OPENSSL_OPENCL_MAX_BUFFER_SIZE")) * 1024 * 1024;
-    }
+  if (getenv("OPENSSL_OPENCL_MAX_BUFFER_SIZE")) {
+      max_buffer_size = atoi(getenv("OPENSSL_OPENCL_MAX_BUFFER_SIZE")) * 1024 * 1024;
+  }
 
-    // dynamic buffer size please
-    buffer_state = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_ALLOC_HOST_PTR, max_buffer_size, NULL, &err);
-    buffer_roundkeys = clCreateBuffer(context, CL_MEM_READ_ONLY, 16 * 15, NULL, &err);
+  // dynamic buffer size please
+  buffer_state = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_ALLOC_HOST_PTR, max_buffer_size, NULL, &err);
+  buffer_roundkeys = clCreateBuffer(context, CL_MEM_READ_ONLY, 16 * 15, NULL, &err);
 
-    err  = clSetKernelArg(encrypt_kernel, 0, sizeof(cl_mem), &buffer_state);
-    err |= clSetKernelArg(encrypt_kernel, 1, sizeof(cl_mem), &buffer_roundkeys);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Error setting arguments\n");
-        return 0;
-    }
+  err  = clSetKernelArg(encrypt_kernel, 0, sizeof(cl_mem), &buffer_state);
+  err |= clSetKernelArg(encrypt_kernel, 1, sizeof(cl_mem), &buffer_roundkeys);
+  if (err != CL_SUCCESS) {
+      fprintf(stderr, "Error setting arguments\n");
+      return 0;
+  }
 
-    printf("Items used: %d\n", local_work_size);
-    printf("init2");
+  printf("Items used: %d\n", local_work_size);
+  printf("init2");
 
-    return 1;
+  return 1;
 }
 
 static int opencl_finish(ENGINE *e) {
@@ -208,70 +208,70 @@ static int opencl_finish(ENGINE *e) {
 }
 
 static int opencl_aes_ecb_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, size_t len) {
-    OPENCL_AES_KEY *dat = (OPENCL_AES_KEY *)ctx->cipher_data;
+  OPENCL_AES_KEY *dat = (OPENCL_AES_KEY *)ctx->cipher_data;
 
-    cl_kernel kernel = dat->kernel;
+  cl_kernel kernel = dat->kernel;
 
-    cl_int err = 0;
+  cl_int err = 0;
 
-    size_t local[1] = {local_work_size};
-    size_t global[1] = {(len / 16 + local[0] - 1) / local[0] * local[0]};
+  size_t local[1] = {local_work_size};
+  size_t global[1] = {(len / 16 + local[0] - 1) / local[0] * local[0]};
 
-    fprintf(stderr, "Global Work Size: %ld\n", global[0]);
-    fprintf(stderr, "Len: %ld\n", len);
+  fprintf(stderr, "Global Work Size: %ld\n", global[0]);
+  fprintf(stderr, "Len: %ld\n", len);
 
-    err = clSetKernelArg(kernel, 2, sizeof(dat->ks.rounds), &dat->ks.rounds);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Error setting arguments\n");
-        return 0;
-    }
+  err = clSetKernelArg(kernel, 2, sizeof(dat->ks.rounds), &dat->ks.rounds);
+  if (err != CL_SUCCESS) {
+    fprintf(stderr, "Error setting arguments\n");
+    return 0;
+  }
 
-    clEnqueueWriteBuffer(queue, buffer_state, CL_FALSE, 0, len, in, 0, NULL, NULL);
-    clEnqueueWriteBuffer(queue, buffer_roundkeys, CL_FALSE, 0, 16 * 15, &dat->ks.rd_key, 0, NULL, NULL);
+  clEnqueueWriteBuffer(queue, buffer_state, CL_FALSE, 0, len, in, 0, NULL, NULL);
+  clEnqueueWriteBuffer(queue, buffer_roundkeys, CL_FALSE, 0, 16 * 15, &dat->ks.rd_key, 0, NULL, NULL);
 
-    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global, local, 0, NULL, NULL);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Error executing kernel: %d\n", err);
-        return 0;
-    }
+  err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global, local, 0, NULL, NULL);
+  if (err != CL_SUCCESS) {
+    fprintf(stderr, "Error executing kernel: %d\n", err);
+    return 0;
+  }
 
-    err = clEnqueueReadBuffer(queue, buffer_state, CL_FALSE, 0, len, out, 0, NULL, NULL);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Error reading results: %d\n", err);
-        return 0;
-    }
+  err = clEnqueueReadBuffer(queue, buffer_state, CL_FALSE, 0, len, out, 0, NULL, NULL);
+  if (err != CL_SUCCESS) {
+    fprintf(stderr, "Error reading results: %d\n", err);
+    return 0;
+  }
 
-    err = clFinish(queue);
-    if (err != CL_SUCCESS) {
-        fprintf(stderr, "Error finishing queue: %d\n", err);
-        return 0;
-    }
+  err = clFinish(queue);
+  if (err != CL_SUCCESS) {
+    fprintf(stderr, "Error finishing queue: %d\n", err);
+    return 0;
+  }
 
-    return 1;
+  return 1;
 }
 
 // delcare the ciphers
 
 static int opencl_aes_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc) {
-    int ret, mode;
-    OPENCL_AES_KEY *dat = (OPENCL_AES_KEY *)ctx->cipher_data;
+  int ret, mode;
+  OPENCL_AES_KEY *dat = (OPENCL_AES_KEY *)ctx->cipher_data;
 
-    // note the key here MUST be BIG endian when represented as integer!!!!
-    mode = ctx->cipher->flags & EVP_CIPH_MODE;
-    if ((mode == EVP_CIPH_ECB_MODE || mode == EVP_CIPH_CBC_MODE) && !enc) {
-        ret = AES_set_decrypt_key(key,ctx->key_len*8,&dat->ks);
-        dat->kernel = decrypt_kernel;
-    } else {
-        ret = AES_set_encrypt_key(key, ctx->key_len * 8, &dat->ks);
-        dat->kernel = encrypt_kernel;
-    }
+  // note the key here MUST be BIG endian when represented as integer!!!!
+  mode = ctx->cipher->flags & EVP_CIPH_MODE;
+  if ((mode == EVP_CIPH_ECB_MODE || mode == EVP_CIPH_CBC_MODE) && !enc) {
+    ret = AES_set_decrypt_key(key,ctx->key_len*8,&dat->ks);
+    dat->kernel = decrypt_kernel;
+  } else {
+    ret = AES_set_encrypt_key(key, ctx->key_len * 8, &dat->ks);
+    dat->kernel = encrypt_kernel;
+  }
 
-    if(ret < 0) {
-        EVPerr(EVP_F_AES_INIT_KEY,EVP_R_AES_KEY_SETUP_FAILED);
-        return 0;
-    }
+  if(ret < 0) {
+    EVPerr(EVP_F_AES_INIT_KEY,EVP_R_AES_KEY_SETUP_FAILED);
+    return 0;
+  }
 
-    return 1;
+  return 1;
 }
 
 BLOCK_CIPHER_generic_pack(NID_aes,128,EVP_CIPH_FLAG_FIPS)
@@ -279,32 +279,32 @@ BLOCK_CIPHER_generic_pack(NID_aes,192,EVP_CIPH_FLAG_FIPS)
 BLOCK_CIPHER_generic_pack(NID_aes,256,EVP_CIPH_FLAG_FIPS)
 
 static int opencl_cipher_nids[] = {
-        NID_aes_128_ecb,
-        NID_aes_192_ecb,
-        NID_aes_256_ecb// more to come !!!
+  NID_aes_128_ecb,
+  NID_aes_192_ecb,
+  NID_aes_256_ecb// more to come !!!
 };
 static int opencl_cipher_nids_number = 3;
 
 static int opencl_ciphers(ENGINE *e, const EVP_CIPHER **cipher, const int **nids, int nid) {
-    if(!cipher) {
-        /* We are returning a list of supported nids */
-        *nids = opencl_cipher_nids;
-        return opencl_cipher_nids_number;
-    }
+  if(!cipher) {
+    /* We are returning a list of supported nids */
+    *nids = opencl_cipher_nids;
+    return opencl_cipher_nids_number;
+  }
 
-    /* We are being asked for a specific cipher */
-    if(nid == NID_aes_128_ecb) {
-        *cipher = &opencl_aes_128_ecb;
-    } else if(nid == NID_aes_192_ecb) {
-        *cipher = &opencl_aes_192_ecb;
-    } else if(nid == NID_aes_256_ecb) {
-        *cipher = &opencl_aes_256_ecb;
-    } else {
-        *cipher = NULL;
-        return 0;
-    }
+  /* We are being asked for a specific cipher */
+  if(nid == NID_aes_128_ecb) {
+    *cipher = &opencl_aes_128_ecb;
+  } else if(nid == NID_aes_192_ecb) {
+    *cipher = &opencl_aes_192_ecb;
+  } else if(nid == NID_aes_256_ecb) {
+    *cipher = &opencl_aes_256_ecb;
+  } else {
+    *cipher = NULL;
+    return 0;
+  }
 
-    return 1;
+  return 1;
 }
 
 // Register the engine
@@ -347,37 +347,37 @@ static int bind_helper(ENGINE *e) {
 
 
 static ENGINE *engine_opencl(void) {
-    ENGINE *ret = ENGINE_new();
+  ENGINE *ret = ENGINE_new();
 
-    if(!ret) {
-        return NULL;
-    }
+  if(!ret) {
+    return NULL;
+  }
 
-    if(!bind_helper(ret)) {
-        ENGINE_free(ret);
-        return NULL;
-    }
+  if(!bind_helper(ret)) {
+    ENGINE_free(ret);
+    return NULL;
+  }
 
-    return ret;
+  return ret;
 }
 
 void ENGINE_load_opencl(void) {
-    ENGINE *toadd = engine_opencl();
-    if(!toadd) return;
-    ENGINE_add(toadd);
-    /* If the "add" worked, it gets a structural reference. So either way,
-     * we release our just-created reference. */
-    ENGINE_free(toadd);
-    ERR_clear_error();
+  ENGINE *toadd = engine_opencl();
+  if(!toadd) return;
+  ENGINE_add(toadd);
+  /* If the "add" worked, it gets a structural reference. So either way,
+    * we release our just-created reference. */
+  ENGINE_free(toadd);
+  ERR_clear_error();
 }
 
 static int bind_engine_opencl(ENGINE *e, const char *id) {
-    if (!bind_helper(e))  {
-        fprintf(stderr, "bind failed\n");
-        return 0;
-    }
+  if (!bind_helper(e))  {
+    fprintf(stderr, "bind failed\n");
+    return 0;
+  }
 
-    return 1;
+  return 1;
 }
 
 IMPLEMENT_DYNAMIC_CHECK_FN()
